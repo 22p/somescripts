a9f6b2b20c5eff704179993c9c432f0e72eece5e
22p <45537698+22p@users.noreply.github.com>
2024-03-23 03:13:19 +0800

    Update update-geoip_cn.sh

--- nftables/tproxy/update-geoip_cn.sh
+++ nftables/tproxy/update-geoip_cn.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-curl -LO https://ispip.clang.cn/all_cn.txt
-mv all_cn.txt geoip4_cn.nft
+curl -Lo geoip4_cn.nft https://ispip.clang.cn/all_cn.txt
 sed -i \
         -e '$!s/$/,/' \
         -e '$s/$/ }\n}/' \
         -e '1s/^/set byp4 {\n  typeof ip daddr\n  flags interval\n  elements = { /' \
         geoip4_cn.nft
 
-curl -LO https://ispip.clang.cn/all_cn_ipv6.txt
-mv all_cn_ipv6.txt geoip6_cn.nft
+curl -Lo geoip6_cn.nft https://ispip.clang.cn/all_cn_ipv6.txt
 sed -i \
         -e '$!s/$/,/' \
         -e '$s/$/ }\n}/' \
