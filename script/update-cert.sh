b7c5827851c37abfe72290cd71e84587cdfa995a
22p <45537698+22p@users.noreply.github.com>
2024-08-17 01:46:12 +0800

    Update update-cert.sh

--- script/update-cert.sh
+++ script/update-cert.sh
@@ -1,12 +1,44 @@
 #!/bin/sh
 
+SRV_NAME="Q"
 GITHUB_TOKEN="github_pat_"
 BARK_TOKEN=""
+TG_BOT_TOKEN=""
+TG_CHAT_ID=""
 REPO_PATH=""
 DOMAIN_NAME=""
 CERTIFICATE_FILE="/opt/podman/cert/$DOMAIN_NAME.crt"
 CERTIFICATE_KEY_FILE="/opt/podman/cert/$DOMAIN_NAME.key"
-RELOAD_CMD="podman exec -it nginx nginx -s reload"
+
+# 将输入的字符串进行 URL 编码
+url_encode() {
+  jq -nr --arg v "$1" '$v|@uri'
+}
+
+# 定义发送 Bark 通知的函数
+# send_bark_notification "标题" "内容" "消息分组" "通知铃声"
+send_bark_notification() {
+  local title=$(url_encode "$1")
+  local message=$(url_encode "$2")
+  local group=$(url_encode "$3")
+  local sound=$4
+  curl -s -o /dev/null "https://api.day.app/$BARK_TOKEN/$title/$message?group=$group&sound=$sound"
+}
+
+# 定义发送 Telegram 通知的函数
+# https://core.telegram.org/bots/api#markdownv2-style
+# send_telegram_notification "标题" "内容"
+send_telegram_notification() {
+  local title=$(url_encode "$1")
+  local message=$(url_encode "$2")
+  curl -s -o /dev/null "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage?chat_id=$TG_CHAT_ID&parse_mode=MarkdownV2&text=%2A$title%2A%0A$message"
+}
+
+# 定义重新加载服务的函数
+reload_service() {
+  # 这里添加需要重新加载的服务
+  podman exec -it nginx nginx -s reload
+}
 
 # 获取当前证书哈希值
 OLD_HASH=$(sha256sum $CERTIFICATE_FILE 2>/dev/null)
@@ -28,14 +60,16 @@ NEW_HASH=$(sha256sum $CERTIFICATE_FILE 2>/dev/null)
 
 if ! grep -q "BEGIN CERTIFICATE" $CERTIFICATE_FILE 2>/dev/null; then
   echo -e "\033[31m证书文件错误，请检查。\033[0m"
-  # 发送失败通知到Bark
-  curl -s -o /dev/null "https://api.day.app/$BARK_TOKEN/SSL%20Download%20Failed/SSL%20download%20has%20failed%20at%20$(date "+%Y-%m-%d%%20%H:%M:%S%%20%Z")?sound=update"
+  send_bark_notification "❌[证书下载失败][$SRV_NAME]" "请检查" "$SRV_NAME" update
+  send_telegram_notification "❌\[证书下载失败\]\[$SRV_NAME\]" "请检查"
   exit 1
 fi
 
 if [ "$OLD_HASH" != "$NEW_HASH" ]; then
   echo "已下载 重新加载服务"
-  $RELOAD_CMD
+  reload_service
+  send_bark_notification "✅[证书更新成功][$SRV_NAME]" "已重新加载服务" "$SRV_NAME" update
+  send_telegram_notification "✅\[证书更新成功\]\[$SRV_NAME\]" "已重新加载服务"
   exit 0
 else
   echo "证书未变动"
