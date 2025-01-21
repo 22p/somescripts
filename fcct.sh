f0c611ea2265a426cbbc28eb9cbf875208685296
22p <45537698+22p@users.noreply.github.com>
2025-01-21 13:48:53 +0800

    10

--- fcct.sh
+++
@@ -1,2 +0,0 @@
-podman run --interactive -v .:/data:z --rm quay.io/coreos/butane:release -d /data  --pretty --strict < router.bu > router.ign
-podman run --interactive -v .:/data:z --rm quay.io/coreos/butane:release -d /data  --pretty --strict < config.bu > config.ign
